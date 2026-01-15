47737d24f508a8fcb689bc53beb10d58da56ae97
islamofking2004 <islamofking2004@gmail.com>
2026-01-15 15:25:00 +0500

    init commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log();
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const helmet = require("helmet");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.get("/getInfo", getInfo)
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+function getInfo(req, res) {
+    res.send("hello world")
+}
