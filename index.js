5b562d9c07cd9e979d0025cf43edbef34f760ae3
islamofking2004 <islamofking2004@gmail.com>
2026-01-15 15:30:06 +0500

    init commit

--- index.js
+++ index.js
@@ -9,19 +9,25 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+let dummy_users = [
+  {
+    name: "islam",
+    age: 67,
+    degree: "student",
+    married: false,
+  },
+];
+
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
 
-app.get("/getInfo", getInfo)
-
-
+app.get("/", getInfo);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-
 function getInfo(req, res) {
-    res.send("hello world")
+  res.send("hello world");
 }
